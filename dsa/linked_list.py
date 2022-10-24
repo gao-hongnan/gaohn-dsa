d306eebcf327c3da82c6fa47aedd14d7aa92bc16
Gao Hongnan <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-10-24 16:25:31 +0800

    docs/refactor: add Min Stack problem and refactor linked list

--- dsa/linked_list.py
+++ dsa/linked_list.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Generic, TypeVar, Any, List, Optional
+from typing import Generic, TypeVar, List, Optional
 
 T = TypeVar("T")
 
@@ -43,6 +43,7 @@ class SinglyLinkedList:
     def __init__(self, node_values: Optional[List[T]] = None) -> None:
         self.head = None
         self.node_values = node_values
+
         if node_values is not None:
             self.add_multiple_nodes()
 
@@ -81,16 +82,42 @@ class SinglyLinkedList:
         return string
 
     def append(self, node_value: T) -> None:
+
         new_node = Node(node_value)
         if self.is_empty():
             self.head = new_node
             return
 
-        temp_node = self.head
+        # the head defines the whole linked list 1->2->3->None
+        temp_node = self.head  # temp_node is now pointing to the first node Node(1)
+        # traverse to the last node
         while temp_node.next_node is not None:
+            # 1st loop, temp_node is Node(1) and temp_node.next_node is Node(2)
+            # so while loop passes
+            # then now temp_node = temp_node.next_node which points is Node(2)
+            # this is equivalent to saying ll.head.next_node which is Node(2)
+            # 2nd loop, temp_node is Node(2) and temp_node.next_node is Node(3)
+            # so while loop passes
+            # then now temp_node = temp_node.next_node which points is Node(3)
+            # this is equivalent to saying ll.head.next_node.next_node which is Node(3)
+            # 3rd loop, temp_node is Node(3) and temp_node.next_node is None
+            # so while loop fails getting us out of the loop
             temp_node = temp_node.next_node
+            # at each loop
+            # we can say temp_node = ll.head.next_node
+            # we can say temp_node = ll.head.next_node.next_node
+            # we can say temp_node = ll.head.next_node.next_node.next_node
+
+        # recall currently temp_node is Node(3) and temp_node.next_node is None
+        # so we set temp_node.next_node to be the new_node
+        # so now Node(3).next_node is Node(4)
+        # this is equivalent to saying ll.head.next_node.next_node.next_node = Node(4)
+        # so now the linked list is 1->2->3->4->None
 
         temp_node.next_node = new_node
+        print(f"new node: {new_node}")
+        print(f"after temp_node: {temp_node}")
+        print(f"after temp node head: {self.head}")
         # self.head = temp_node
 
     def __repr__(self) -> str:
@@ -129,17 +156,19 @@ if __name__ == "__main__":
     ll.head.next_node.next_node = third
     print(ll)
 
-    values = [1, 2, 3]
-    ll = SinglyLinkedList(values)
-    print(ll)
+    # values = [1, 2, 3]
+    # ll = SinglyLinkedList(values)
+    # print(ll)
 
     # # print(ll.head.next_node.next_node.next_node)
     # # print(ll.traverse())
     # print(ll.size)
 
-    # fourth = Node(4)
-    # ll.append(fourth)
-
+    fourth = 4
+    ll.append(fourth)
+    # ll.append_v2(fourth)
+    print(ll)
+    print(ll.head)
     # print(ll.head)
     # print(ll.size)
     # # print(ll.head)
