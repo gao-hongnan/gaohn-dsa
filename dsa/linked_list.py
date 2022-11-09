6f3764ee0dd0dee6bc4281ff3a1c1595de5f6d52
Gao Hongnan <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-11-09 13:14:30 +0800

    docs: updated linked_list

--- dsa/linked_list.py
+++ dsa/linked_list.py
@@ -38,10 +38,23 @@ class Node(Generic[T]):
 
 
 class SinglyLinkedList:
-    head: Node  # FIXME: self.head = None does not match this type.
+    """
+    The SinglyLinkedList object is initialized with a head node.
+
+    The `head` node (the first node) of a **Linked List** is of a `Node` object.
+    The `head` **entirely determines** the entirety of the whole **Linked List**.
+    Because knowing the head node of the **Linked List**, we will be able to
+    know every single node that comes after it sequentially (if exists).
+
+    Attributes:
+        head (Node): The head node of the linked list.
+    """
+
+    head: Node
+    node_values: List[T]
 
     def __init__(self, node_values: Optional[List[T]] = None) -> None:
-        self.head = None
+        self.head = None  # should really be Node(None) in case mypy complains
         self.node_values = node_values
 
         if node_values is not None:
@@ -81,44 +94,61 @@ class SinglyLinkedList:
                 string += "None"
         return string
 
+    # def append(self, node_value: T) -> None:
+
+    #     new_node = Node(node_value)
+    #     if self.is_empty():
+    #         self.head = new_node
+    #         return
+
+    #     # the head defines the whole linked list 1->2->3->None
+    #     temp_node = self.head  # temp_node is now pointing to the first node Node(1)
+    #     # traverse to the last node
+    #     while temp_node.next_node is not None:
+    #         # 1st loop, temp_node is Node(1) and temp_node.next_node is Node(2)
+    #         # so while loop passes
+    #         # then now temp_node = temp_node.next_node which points is Node(2)
+    #         # this is equivalent to saying ll.head.next_node which is Node(2)
+    #         # 2nd loop, temp_node is Node(2) and temp_node.next_node is Node(3)
+    #         # so while loop passes
+    #         # then now temp_node = temp_node.next_node which points is Node(3)
+    #         # this is equivalent to saying ll.head.next_node.next_node which is Node(3)
+    #         # 3rd loop, temp_node is Node(3) and temp_node.next_node is None
+    #         # so while loop fails getting us out of the loop
+    #         temp_node = temp_node.next_node
+    #         # at each loop
+    #         # we can say temp_node = ll.head.next_node now points to Node(2)
+    #         # we can say temp_node = ll.head.next_node.next_node now points to Node(3)
+    #         # we can say temp_node = ll.head.next_node.next_node.next_node now points to None
+
+    #     # recall currently temp_node is Node(3) and temp_node.next_node is None
+    #     # so we set temp_node.next_node to be the new_node
+    #     # so now Node(3).next_node is Node(4)
+    #     # this is equivalent to saying ll.head.next_node.next_node.next_node = Node(4)
+    #     # so now the linked list is 1->2->3->4->None
+
+    #     temp_node.next_node = new_node
+    #     print(f"new node: {new_node}")
+    #     print(f"after temp_node: {temp_node}")
+    #     print(f"after temp node head: {self.head}")
+    #     # self.head = temp_node
+
     def append(self, node_value: T) -> None:
 
         new_node = Node(node_value)
         if self.is_empty():
             self.head = new_node
             return
+        # temp_node = self.head
+        while self.head.next_node is not None:
+            print(1)
+            self.head = self.head.next_node
+            # self.head = self.head.next_node
+            # self.head = self.head.next_node.next_node
 
-        # the head defines the whole linked list 1->2->3->None
-        temp_node = self.head  # temp_node is now pointing to the first node Node(1)
-        # traverse to the last node
-        while temp_node.next_node is not None:
-            # 1st loop, temp_node is Node(1) and temp_node.next_node is Node(2)
-            # so while loop passes
-            # then now temp_node = temp_node.next_node which points is Node(2)
-            # this is equivalent to saying ll.head.next_node which is Node(2)
-            # 2nd loop, temp_node is Node(2) and temp_node.next_node is Node(3)
-            # so while loop passes
-            # then now temp_node = temp_node.next_node which points is Node(3)
-            # this is equivalent to saying ll.head.next_node.next_node which is Node(3)
-            # 3rd loop, temp_node is Node(3) and temp_node.next_node is None
-            # so while loop fails getting us out of the loop
-            temp_node = temp_node.next_node
-            # at each loop
-            # we can say temp_node = ll.head.next_node
-            # we can say temp_node = ll.head.next_node.next_node
-            # we can say temp_node = ll.head.next_node.next_node.next_node
+        print(self.head)
 
-        # recall currently temp_node is Node(3) and temp_node.next_node is None
-        # so we set temp_node.next_node to be the new_node
-        # so now Node(3).next_node is Node(4)
-        # this is equivalent to saying ll.head.next_node.next_node.next_node = Node(4)
-        # so now the linked list is 1->2->3->4->None
-
-        temp_node.next_node = new_node
-        print(f"new node: {new_node}")
-        print(f"after temp_node: {temp_node}")
-        print(f"after temp node head: {self.head}")
-        # self.head = temp_node
+        self.head.next_node = new_node
 
     def __repr__(self) -> str:
         return self.traverse()
