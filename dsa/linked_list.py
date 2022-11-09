ddee2dfc296fcd065f541302199d17074955062e
Gao Hongnan <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-11-09 18:22:09 +0800

    docs: update linked_list

--- dsa/linked_list.py
+++ dsa/linked_list.py
@@ -60,6 +60,15 @@ class SinglyLinkedList:
         if node_values is not None:
             self.add_multiple_nodes()
 
+    def __repr__(self) -> str:
+        return self.traverse()
+
+    def __iter__(self) -> None:
+        temp = self.head
+        while temp is not None:
+            yield temp, temp.curr_node_value  # yield the node and the node value
+            temp = temp.next_node
+
     def is_empty(self) -> bool:
         """Check if the linked list is empty.
 
@@ -94,64 +103,84 @@ class SinglyLinkedList:
                 string += "None"
         return string
 
-    # def append(self, node_value: T) -> None:
+    def insert(self, node_value: T) -> None:
+        """Insert a node at the beginning of the linked list.
 
-    #     new_node = Node(node_value)
-    #     if self.is_empty():
-    #         self.head = new_node
-    #         return
+        Time Complexity:
+            O(1) since we are inserting at the beginning.
 
-    #     # the head defines the whole linked list 1->2->3->None
-    #     temp_node = self.head  # temp_node is now pointing to the first node Node(1)
-    #     # traverse to the last node
-    #     while temp_node.next_node is not None:
-    #         # 1st loop, temp_node is Node(1) and temp_node.next_node is Node(2)
-    #         # so while loop passes
-    #         # then now temp_node = temp_node.next_node which points is Node(2)
-    #         # this is equivalent to saying ll.head.next_node which is Node(2)
-    #         # 2nd loop, temp_node is Node(2) and temp_node.next_node is Node(3)
-    #         # so while loop passes
-    #         # then now temp_node = temp_node.next_node which points is Node(3)
-    #         # this is equivalent to saying ll.head.next_node.next_node which is Node(3)
-    #         # 3rd loop, temp_node is Node(3) and temp_node.next_node is None
-    #         # so while loop fails getting us out of the loop
-    #         temp_node = temp_node.next_node
-    #         # at each loop
-    #         # we can say temp_node = ll.head.next_node now points to Node(2)
-    #         # we can say temp_node = ll.head.next_node.next_node now points to Node(3)
-    #         # we can say temp_node = ll.head.next_node.next_node.next_node now points to None
+        Args:
+            node_value (T): The value of the node to be inserted at the beginning of the linked list.
+        """
+        new_node = Node(node_value)
+        if self.is_empty():
+            self.head = new_node
+            return
 
-    #     # recall currently temp_node is Node(3) and temp_node.next_node is None
-    #     # so we set temp_node.next_node to be the new_node
-    #     # so now Node(3).next_node is Node(4)
-    #     # this is equivalent to saying ll.head.next_node.next_node.next_node = Node(4)
-    #     # so now the linked list is 1->2->3->4->None
-
-    #     temp_node.next_node = new_node
-    #     print(f"new node: {new_node}")
-    #     print(f"after temp_node: {temp_node}")
-    #     print(f"after temp node head: {self.head}")
-    #     # self.head = temp_node
+        new_node.next_node = self.head
+        self.head = new_node
 
     def append(self, node_value: T) -> None:
+        """Append a node to the end of the linked list.
+
+        Note:
+            If our ll.head is 1 -> 2 -> 3 -> None, and we want to append 4,
+            at each loop
+            loop 1: we can say temp_node = ll.head.next_node now points to Node(2)
+            loop 2: we can say temp_node = ll.head.next_node.next_node now points to Node(3)
+            there is no loop 3 because temp_node.next_node.next_node is None after loop 2
+            i.e. we can say temp_node = ll.head.next_node.next_node.next_node now points to None
+
+            Recall currently temp_node is Node(3) and temp_node.next_node is None
+            so we set temp_node.next_node to be the new_node
+            so now Node(3).next_node is Node(4)
+            this is equivalent to saying ll.head.next_node.next_node.next_node = Node(4)
+            so now the linked list is 1 -> 2 -> 3 -> 4 -> None
+
+        Time Complexity:
+            O(n) since need traverse. If we had a tail pointer, we could do O(1)?
+
+        Args:
+            node_value (T): The value of the node to be appended to the linked list.
+        """
 
         new_node = Node(node_value)
         if self.is_empty():
             self.head = new_node
             return
-        # temp_node = self.head
-        while self.head.next_node is not None:
-            print(1)
+
+        # the head defines the whole linked list 1->2->3->None
+        temp_node = self.head  # temp_node is now pointing to the first node Node(1)
+        # traverse to the last node
+        while temp_node.next_node is not None:
+            temp_node = temp_node.next_node
+
+        temp_node.next_node = new_node
+
+    def remove(self, node_value_to_remove: T) -> None:
+        """Remove a node from the linked list. Note it removes the first occurrence of the node."""
+        if self.is_empty():
+            raise ValueError("Cannot remove from an empty linked list.")
+
+        if self.head.curr_node_value == node_value_to_remove:  # first case
+            # set the head to the next node, essentially removing the first node
             self.head = self.head.next_node
-            # self.head = self.head.next_node
-            # self.head = self.head.next_node.next_node
+            return
 
-        print(self.head)
+        previous_node = self.head
+        for node, node_value in self:
+            if node_value == node_value_to_remove:
+                previous_node.next_node = (
+                    node.next_node
+                )  # link the previous node to the next node
+                return
 
-        self.head.next_node = new_node
+            previous_node = node  # if the if clause is not true, we continue searching by updating the previous_node to be the current node
 
-    def __repr__(self) -> str:
-        return self.traverse()
+        raise ValueError(f"{node_value_to_remove} not found in linked list.")
+
+    def search(self):
+        pass
 
     @property
     def size(self) -> int:
@@ -185,6 +214,8 @@ if __name__ == "__main__":
     ll.head.next_node = second
     ll.head.next_node.next_node = third
     print(ll)
+    for node, node_value in ll:
+        print(node, node_value)
 
     # values = [1, 2, 3]
     # ll = SinglyLinkedList(values)
@@ -199,6 +230,6 @@ if __name__ == "__main__":
     # ll.append_v2(fourth)
     print(ll)
     print(ll.head)
-    # print(ll.head)
-    # print(ll.size)
-    # # print(ll.head)
+
+    ll.remove(4)
+    print(ll.head)
